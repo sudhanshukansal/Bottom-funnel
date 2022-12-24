2c423b974a2898a2cd45c602bd4216205a4dec40
sudhanshu <sudhanshukansal09@gmail.com>
2022-12-24 22:14:04 +0530

    Remove comment from utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,24 +1,3 @@
-// var tempArr=[];
-// function check(number) {
-//   for(var i= 0 ; i<number.length;i++){
-
-//      var r,result=0,temp=number[i];
-//     while (number[i] > 0) {
-//       r = number[i] % 10;
-//       number[i] = parseInt(number[i]/10);
-//       result  = result*10+r;
-
-//     }
-//     if (result === temp){
-//       tempArr.push(result)
-//     }
-
-//   }
-
-//   }
-
-// check(props.number);
-// console.log(tempArr)
 
 export const CheckPalindrome = (number) => {
   var r,
